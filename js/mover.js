30fbf8d1d1c7120e5bb21a4c3ecab441751e84f6
Chris <celie56@gmail.com>
2013-09-30 23:57:53 -0700

    Mover moved

--- js/mover.js
+++
@@ -1,9 +0,0 @@
-var message = document.getElementById("message");
-message.innerHTML = "Press Enter to Begin";
-
-keypress(enter,function(){
-	var canvas=document.getElementById("Canvas");
-	var ctx=canvas.getContext("2d");
-	ctx.fillStyle="#FF0000";
-	ctx.fillRect(0,0,150,75);
-});
