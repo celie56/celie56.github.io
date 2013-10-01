0ac85639d2e55ea4839d9113187d0a00fb5bb174
Chris <celie56@gmail.com>
2013-09-30 18:34:55 -0700

    Working on mover

--- js/mover.js
+++ js/mover.js
@@ -1,2 +1,9 @@
 var message = document.getElementById("message");
-message.innerHTML = "Press Start to Begin";
+message.innerHTML = "Press Enter to Begin";
+
+keypress(enter,function(){
+	var canvas=document.getElementById("Canvas");
+	var ctx=canvas.getContext("2d");
+	ctx.fillStyle="#FF0000";
+	ctx.fillRect(0,0,150,75);
+});
