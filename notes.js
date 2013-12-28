f58740c8a956817013c2f136a8f89233bed4241b
Chris <celie56@gmail.com>
2013-12-28 10:29:46 -0800

    Postin' Notes

--- notes.js
+++ notes.js
@@ -1,8 +1,12 @@
 {
-    "text": "First post. Let's see how this works.",
-    "date": "12/27/13"
-},
-{
-    "text": "Try number two, still no idea",
-    "date": "12/27/13"
+    "entries": [
+        {
+            "text": "First post. Let's see how this works.",
+            "date": "12/27/13"
+        },
+        {
+            "text": "Try number two, still no idea",
+            "date": "12/27/13"
+        }
+    ]
 }
