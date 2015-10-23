d925b0511107729954028de27373ffe04afc4548
celie56 <celie56@gmail.com>
2015-10-22 22:34:02 -0400

    updated course list

--- js/courses.js
+++ js/courses.js
@@ -2,4 +2,4 @@
 var options = { valueNames: ['order', 'department', 'desc', 'num']};
 var courses = new List('courses', options);
 
-courses.sort('order', {order: 'asc'});
+//courses.sort('order', {order: 'asc'});
